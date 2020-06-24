72f10f445e8170c573aafabf622551f172d2cfe5
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-06-24 13:09:44 -0400

    Added submodule_init.bat

---
+++ submodule_init.bat
@@ -0,0 +1,3 @@
+git submodule add https://github.com/DaelonSuzuka/K42-Peripheral-Libraries.git src/peripherals
+git submodule add https://github.com/DaelonSuzuka/K42-System-Libraries.git src/os
+git submodule add https://github.com/DaelonSuzuka/Easy-XC8.git toolchain
