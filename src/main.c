2316c0e36eec312fd3968e08ae40fb838f79207d
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-08-25 12:23:54 -0400

    Added macros to identify compiler driver and standard

--- src/main.c
+++ src/main.c
@@ -3,6 +3,27 @@
 
 /* ************************************************************************** */
 
+#ifdef __XC8_CC_C99__
+
+// Disable "unused variable" warning
+#pragma warning disable 1090
+
+// Disable warning spam about reset vectors
+#pragma warning disable 2020
+
+// Disable warning spam about unused functions
+#pragma warning disable 520
+
+// Disable "expression generates no code"
+#pragma warning disable 759
+
+// Disable "pointer in expression may have no targets"
+#pragma warning disable 1498
+
+#endif // __XC8_CC_C99__
+
+/* ************************************************************************** */
+
 void main(void) {
     startup();
 
