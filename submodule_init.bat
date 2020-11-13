ae1bd951b682210c81d6ee92395e8e88833ce0a1
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-11-13 02:05:02 -0500

    Changed init script to use ssh links

--- submodule_init.bat
+++ submodule_init.bat
@@ -1,3 +1,3 @@
-git submodule add https://github.com/DaelonSuzuka/K42-Peripheral-Libraries.git src/peripherals
-git submodule add https://github.com/DaelonSuzuka/K42-System-Libraries.git src/os
-git submodule add https://github.com/DaelonSuzuka/Easy-XC8.git toolchain
+git submodule add git@github.com:DaelonSuzuka/K42-Peripheral-Libraries.git src/peripherals
+git submodule add git@github.com:DaelonSuzuka/K42-System-Libraries.git src/os
+git submodule add git@github.com:DaelonSuzuka/Easy-XC8.git toolchain
